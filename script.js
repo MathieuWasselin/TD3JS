8a93c7f07a8131c5740e7c683a000137b8d1f474
MathieuWasselin <wasselinmathieu@gmail.com>
2021-11-09 09:58:17 +0100

    changement3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Action pour bouton accepter = Barre de cookie qui disparait.
 document.getElementById("buttonAccept").addEventListener("click", hideModal) // hidemodal = fonction que l'on nomme
 function hideModal(){
-document.getElementById("footer").style.display = "none";  
+document.getElementById("footer").style.display = "none";  // instruction que l'on donne à la fonction = disparition du footer (barre de cookie) à l'evenement "click".
 }
 // Action pour bouton Personnaliser = Fenetre modale qui apparait.
 document.getElementById("buttonPersonalize").addEventListener("click", showModal) 
@@ -13,32 +13,32 @@ document.getElementById("fenetremodale").style.display = "block";
 function changeColorAccept(){
 this.style.backgroundColor = "green";
 let buttonReverse = this.getAttribute("data-inverse");
-document.getElementById(buttonReverse).style.backgroundColor = "#D3D3D3";
+document.getElementById(buttonReverse).style.backgroundColor = "#D3D3D3"; // Bouton qui redevient gris lors du click du bouton d'en face.
 
 
 }
 
-document.getElementById("acceptContent").addEventListener("click", changeColorAccept);
+document.getElementById("acceptContent").addEventListener("click", changeColorAccept); // Bouton acceptContent qui prend la couleur verte au "click".
 
-document.getElementById("acceptPublicity").addEventListener("click", changeColorAccept);
+document.getElementById("acceptPublicity").addEventListener("click", changeColorAccept); // Bouton acceptPublicity qui prend la couleur verte au "click".
 
-document.getElementById("acceptMeasure").addEventListener("click", changeColorAccept);
+document.getElementById("acceptMeasure").addEventListener("click", changeColorAccept); // Bouton acceptMeasure qui prend la couleur verte au "click".
 
 
 function changeColorDeclined(){
 
 this.style.backgroundColor = "red";
 let buttonReverse = this.getAttribute("data-inverse");
-document.getElementById(buttonReverse).style.backgroundColor = "#D3D3D3";
+document.getElementById(buttonReverse).style.backgroundColor = "#D3D3D3"; 
 
     
 
     }
     
-    document.getElementById("refuseContent").addEventListener("click", changeColorDeclined);
+    document.getElementById("refuseContent").addEventListener("click", changeColorDeclined); // Bouton refuseContent qui prend la couleur rouge au "click".
     
-    document.getElementById("refusePublicity").addEventListener("click", changeColorDeclined);
+    document.getElementById("refusePublicity").addEventListener("click", changeColorDeclined); // Bouton refusePublicity qui prend la couleur rouge au "click".
     
-    document.getElementById("refuseMeasure").addEventListener("click", changeColorDeclined);
+    document.getElementById("refuseMeasure").addEventListener("click", changeColorDeclined); // Bouton refuseMeasure qui prend la couleur rouge au "click".
     
 
